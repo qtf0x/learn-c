038f2f13d7f3218830d850117e91c5cd5984464a
Vincent Marias <vmarias@mines.edu>
2022-05-15 18:36:21 -0600

    Hello, world!

---
+++ src/HelloWorld.c
@@ -0,0 +1,15 @@
+/**
+ * @file HelloWorld.c
+ * @author Vincent Marias (@vincent-marias)
+ * @date 2022-05-15
+ *
+ * @brief Hello, world!
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!");
+
+    return 0;
+}

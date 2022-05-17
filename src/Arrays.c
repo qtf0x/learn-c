a54ed75cfe7e0c780572d2446a507e044b314515
Vincent Marias <vmarias@mines.edu>
2022-05-16 20:14:56 -0600

    arrays

---
+++ src/Arrays.c
@@ -0,0 +1,28 @@
+/**
+ * @file Arrays.c
+ * @author Vincent Marias (@vincent-marias)
+ * @date 2022-05-16
+ *
+ * @brief Basic array syntax.
+ */
+
+#include <stdio.h>
+
+int main() {
+    // defines an array of 10 integers
+    int numbers[10];
+
+    // populate the array
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    numbers[3] = 40;
+    numbers[4] = 50;
+    numbers[6] = 60;
+    numbers[7] = 70;
+
+    // print the 7th number from the array, which has an index of 6
+    printf("The 7th number in the array is %d\n", numbers[6]);
+
+    return 0;
+}

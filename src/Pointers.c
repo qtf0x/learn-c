51ec8fccfbd8addf4de89d7b72c8812c8de63a3a
Vincent Marias <vmarias@mines.edu>
2022-05-20 21:03:15 -0600

    pointers

---
+++ src/Pointers.c
@@ -0,0 +1,37 @@
+/**
+ * @file Pointers.c
+ * @author Vincent Marias (@vincent-marias)
+ * @date 2022-05-20
+ *
+ * @brief
+ */
+
+#include <stdio.h>
+
+int main() {
+    // arrays and pointers are the same thing!
+    char* name = "John";
+
+    printf("Name: %s\n", name);
+
+    printf("Name: ");
+    for (int i = 0; i < 4; ++i) {
+        printf("%c", name[i]);
+    }
+    printf("\n");
+
+    // accessing pointers
+    int a = 1;
+    int* pointer_to_a = &a;
+
+    // let's change the variable a
+    a += 1;
+
+    // we just changed the variable again!
+    *pointer_to_a += 1;
+
+    // will print out 3
+    printf("The value of a is now %d\n", a);
+
+    return 0;
+}

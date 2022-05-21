9434716253b5581f850c1440b5d472c6bc56a115
Vincent Marias <vmarias@mines.edu>
2022-05-20 20:09:17 -0600

    functions

---
+++ src/Functions.c
@@ -0,0 +1,22 @@
+/**
+ * @file Functions.c
+ * @author Vincent Marias (@vincent-marias)
+ * @date 2022-05-20
+ *
+ * @brief Functions are FUN.
+ */
+
+/* function declaration */
+int foo(int bar);
+
+int main() {
+    // functions receive either a fixed or variable amount of arguments
+    // functions can only return one value, or return no value
+
+    /* calling foo from main */
+    printf("The value of foo is %d\n", foo(1));
+
+    return 0;
+}
+
+int foo(int bar) { return bar + 1; }
